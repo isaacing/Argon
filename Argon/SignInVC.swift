bda742f7a5f8453d9394231e09f2f008147f2c56
isaacing <ijingalls5@gmail.com>
2016-10-07 17:30:34 -0500

    Completed setting up Firebase integration

---
+++ Argon/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Argon
+//
+//  Created by Isaac Ingalls on 10/5/16.
+//  Copyright © 2016 Isaac Ingalls. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Argon/Pods-Argon-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ArgonVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ArgonVersionString[];
+
